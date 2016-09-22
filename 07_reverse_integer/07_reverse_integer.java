f278c99583f51c0251f8c7f8d47fb460b4444a33
wht931011 <wht931011@gmail.com>
2016-09-21 22:43:19 -0400

    solution to question 07

---
+++ 07_reverse_integer/07_reverse_integer.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int reverse(int x) {
+        long result = 0;
+        int tmp = Math.abs(x);
+        while(tmp>0){
+            result *= 10;
+            result += tmp % 10;
+            if(result > Integer.MAX_VALUE){
+                return 0;
+            }
+            tmp /= 10;
+        }
+        return (int)(x>=0?result:-result);
+    }
+}
